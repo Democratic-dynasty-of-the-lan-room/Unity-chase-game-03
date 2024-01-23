48109764b407ca0c40eeeb8807578bbea6258aef
Dendrose <Dendrose10@gmail.com>
2024-01-23 15:47:48 +0200

    First Enemy Script
    Added wandering state going back to territory state etc.

--- Unity Project/Assets/Code/Scripts/SampleScene/EnemyController.cs
+++ Unity Project/Assets/Code/Scripts/SampleScene/EnemyController.cs
@@ -1,10 +1,16 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
+using static UnityEditor.Handles;
 
 namespace Code.Scripts.SampleScene
 {
     public class EnemyController : MonoBehaviour
     {
+        [SerializeField] GameObject FirstEnemyTerritory;
+
+        [SerializeField] GameObject LastPlayerPosition;
 
         [SerializeField] GameObject Player;
 
@@ -12,13 +18,59 @@ namespace Code.Scripts.SampleScene
 
         [SerializeField] GameObject InventoryScript;
 
+        [SerializeField] LayerMask WhatIsGround;
+
+
+
         Transform target;
         NavMeshAgent agent;
 
 
+
         public float lookRadius = 10;
 
+        private float distance;
+
         public GameObject RestartMenu;
+
+        bool InTerritory;
+
+
+
+        //State Machine  
+
+        [Header("Chasing state")]
+
+        public float ChasingSpeed;
+
+
+
+        // WanderState
+        [Header("Wander state")]
+
+        public GameObject WanderPosition;    
+
+        public float WanderDistance;
+
+        public float RandomUpperWanderWaitTime;
+
+        public float UpperWanderOutsideTerritory;
+
+        public float LowerWanderOutsideTerritory;
+
+        public float WanderSpeed;
+
+
+
+        private bool IsChasing = false;
+
+        private bool IsWandering = true;
+
+        private bool IsGoingToTeritory = false;
+
+        private bool CanGoBackToTerritory;
+
+        private bool CanStartCoroutine;
 
         //Check if enemy and player collide
         void OnCollisionEnter(Collision collision)
@@ -41,43 +93,156 @@ namespace Code.Scripts.SampleScene
             }
         }
 
-
-
-
         // Start is called before the first frame update
         void Start()
         {
             target = PlayerManager.instance.player.transform;
             agent = GetComponent<NavMeshAgent>();
 
-       
+            CanStartCoroutine = true;
 
-    
+            CanGoBackToTerritory = false;
 
             //Making sure Player is set to true? I guess
             Player.SetActive(true);
 
             //this script I think
             this.enabled = true;
-
-            ;
         }
 
         // Update is called once per frame
         void Update()
         {
-            float distance = Vector3.Distance(target.position, transform.position);
+            distance = Vector3.Distance(target.position, transform.position);
+
+            if (IsChasing)
+            {
+                ChasingState();
+            }
+            else if (IsWandering)
+            {
+                WanderingState();
+            }
+            else if (IsGoingToTeritory)
+            {
+                GoingToTerritoryState();
+            }           
+        }
+
+        private void ChasingState()
+        {
+            agent.speed = ChasingSpeed;
+
+            if (agent.velocity == new Vector3(0, 0, 0))
+            {
+                IsWandering = true;
+
+                IsChasing = false;
+            }
+
+            //Enemy Is in i'ts territory
+            if (InTerritory)
+            {          
+                if (distance <= lookRadius)
+                {
+                    agent.SetDestination(target.position);
+
+                    LastPlayerPosition.transform.position = Player.transform.position;
+
+                    if (distance <= agent.stoppingDistance)
+                    {
+                        //Attack
+                        //face target
+                        FaceTarget();
+                    }
+                }
+            }
+            //Enemy Is out it's Territory
+            else if (!InTerritory)
+            {
+
+                if (distance <= lookRadius)
+                {
+                    agent.SetDestination(target.position);
+
+                    LastPlayerPosition.transform.position = Player.transform.position;
+
+                    if (distance <= agent.stoppingDistance)
+                    {
+                        //Attack
+                        //face target
+                        FaceTarget();
+                    }
+                }
+                else if (distance >= lookRadius)
+                {
+                    agent.SetDestination(LastPlayerPosition.transform.position);                        
+                }
+            }
+        }
+
+        private void WanderingState()
+        {
+            agent.speed = WanderSpeed;
 
             if (distance <= lookRadius)
             {
-                agent.SetDestination(target.position);
+                IsChasing = true;
 
-                if (distance <= agent.stoppingDistance)
+                IsWandering = false;
+            }
+
+            if (!InTerritory)
+            {
+                StartCoroutine(WaitBeforeGoingToTerritory());
+
+                if (CanGoBackToTerritory)
                 {
-                    //Attack
-                    //face target
-                    FaceTarget();
+                    IsGoingToTeritory = true;
+
+                    IsWandering = false;
+
+                    CanGoBackToTerritory = false;
+                }         
+            }
+
+            if (CanStartCoroutine == true)
+            {
+                WanderPosition.transform.position = UnityEngine.Random.insideUnitSphere * WanderDistance + transform.position;
+            }
+           
+            if (Physics.Raycast(WanderPosition.transform.position, new Vector3(0, -0.5f, 0), WhatIsGround))
+            {
+                if (CanStartCoroutine == true)
+                {
+                    StartCoroutine(WanderWaitTimeCoroutine());
                 }
+
+                CanStartCoroutine = false;
+
+                agent.SetDestination(WanderPosition.transform.position);                            
+            }
+            
+        }
+
+        private void GoingToTerritoryState()
+        {
+            agent.speed = ChasingSpeed;
+
+            agent.SetDestination(FirstEnemyTerritory.transform.position);
+
+            if (agent.velocity == new Vector3(0, 0, 0))
+            {
+                IsWandering = true;
+
+                IsGoingToTeritory = false;
+            }
+
+            if(distance <= lookRadius) 
+            {
+                IsChasing = true;
+
+                IsGoingToTeritory = false;
             }
         }
 
@@ -86,15 +251,47 @@ namespace Code.Scripts.SampleScene
             Vector3 direction = (target.position - transform.position).normalized;
             Quaternion lookRotation = Quaternion.LookRotation(new Vector3 (direction.x, 0, direction.z));
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);  
+        }     
+
+        //Enemy Enters i'ts territory
+        private void OnTriggerEnter(Collider other)
+        {
+
+            if (other.gameObject.tag == "EnemyTerritory")
+            {
+                InTerritory = true;                                                              
+            }
         }
 
+        //Enemy Leaves it's Territory
+        private void OnTriggerExit(Collider other)
+        {          
+
+            if (other.gameObject.tag == "EnemyTerritory")
+            {
+                InTerritory = false;
+            }       
+        }
 
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(transform.position, lookRadius);
-
         }
 
-    }
+        //Wait Random amount of time before coroutine can be started again
+        private IEnumerator WanderWaitTimeCoroutine()
+        { 
+            yield return new WaitForSeconds(Random.Range(0, RandomUpperWanderWaitTime));
+
+            CanStartCoroutine = true;      
+        }
+
+        private IEnumerator WaitBeforeGoingToTerritory()
+        {
+            yield return new WaitForSeconds(Random.Range(LowerWanderOutsideTerritory, UpperWanderOutsideTerritory));
+
+            CanGoBackToTerritory = true;
+        }
+    }  
 }
