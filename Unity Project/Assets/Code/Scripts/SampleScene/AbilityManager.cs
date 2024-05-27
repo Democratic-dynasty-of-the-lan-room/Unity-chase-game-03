654280964ae01f36bf54f8e072b83efe2db5f8f6
Dendrose <Dendrose10@gmail.com>
2024-05-27 10:07:04 +0200

    Saving inventory almost
    Still one error.

--- Unity Project/Assets/Code/Scripts/SampleScene/AbilityManager.cs
+++ Unity Project/Assets/Code/Scripts/SampleScene/AbilityManager.cs
@@ -2,6 +2,7 @@ using Code.Scripts.SampleScene.Player;
 using JetBrains.Annotations;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
@@ -9,6 +10,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+using static Unity.VisualScripting.Member;
 
 public class AbilityManager : MonoBehaviour, IDataPersistence
 {
@@ -60,14 +62,14 @@ public class AbilityManager : MonoBehaviour, IDataPersistence
 
             if (slot.childCount > 0)
             {
-                print("ChildCount" + slot.childCount);
+                //print("ChildCount" + slot.childCount);
 
                 GameObject itemObj = slot.GetChild(0).gameObject;
 
                 //Save name of Object to reference when instantiating it later.
                 SaveInventoryName[i] = itemObj.gameObject.name;
 
-                Debug.Log("ItemObject Name: " + itemObj.name);
+                //Debug.Log("ItemObject Name: " + itemObj.name);
 
                 SaveInventorySlotIndex[i] = i;
 
@@ -90,6 +92,11 @@ public class AbilityManager : MonoBehaviour, IDataPersistence
                 //Debug.Log("Slot");
                 // we have children!
             }
+            else if (slot.childCount == 0)
+            {
+                SaveInventoryName[i] = "";
+                Debug.Log("SaveInventoryName = nothing");
+            }
         }
     }
 
@@ -100,50 +107,83 @@ public class AbilityManager : MonoBehaviour, IDataPersistence
 
     public void LoadInventoryQuestion()
     {
-        Debug.Log("RunLoadInventory");
+        //Debug.Log("RunLoadInventory");
 
         // Check if this is working correctly!
         for (int i = 0; i < inventory.slots.Length; i++)
         {
             Debug.Log("ForLoop");
-            foreach (Transform slot in inventory.slots[i].transform)
+
+            Debug.Log("ForeachChild");
+            if (inventory.slots[i].transform.childCount > 0)
             {
-                Debug.Log("ForeachChild");
-                if (slot.childCount > 0)
-                {
-                    Debug.Log("DestroyChildren");
-                    Destroy(slot.GetChild(0).gameObject);
-                }
-            }         
-        }
-       
-        for (int i = 0; i < inventory.slots.Length; i++)
-        {
-            GameObject instance = Instantiate(Resources.Load("Inventory UI/ImageDash", typeof(GameObject))) as GameObject;
+                print("ChildCount" + inventory.slots[i].transform.childCount);
 
-            Transform slot = inventory.slots[i].transform;
-
-            if (instance != null)
-            {
-                //instance.transform.position = slot.transform.position;
-
-                instance.transform.SetParent(slot.transform);
-
-                //instance.transform.localPosition = new Vector3(slot.transform.position.x, slot.transform.position.y, slot.transform.position.z);
-
-                instance.transform.localPosition = new Vector3(0, 0, 0);
-
-                Debug.Log("Set pos of instantiated object");
+                Debug.Log("DestroyChildren");
+                Destroy(inventory.slots[i].transform.GetChild(0).gameObject);
+                Debug.Log("There is an error: Transform child out of bounds for Image speed boost?");
             }
             else
             {
-                Debug.LogError("Failed to load and instantiate the GameObject.");
+                print("ChildCount" + inventory.slots[i].transform.childCount);
+
+                Debug.Log("There was no inventory to destroy");
             }
+        }
+       
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {     
+            string SlotName = SaveInventoryName[i].Replace("(Clone)", "");
+
+            Debug.Log(SlotName);
+      
+            if (SlotName != null)
+            {
+                // This is not quite ideal because it will remove any part of a name that has clone in it. Which means it won't work for objects that happen to have clone in the name already.
+                GameObject instance = Instantiate(Resources.Load("Inventory UI/" + SlotName, typeof(GameObject))) as GameObject;
+
+                Transform slotTransform = inventory.slots[i].transform;
+
+                if (instance != null)
+                {
+                    // sets the instance to be a parent of sloTransform
+                    instance.transform.SetParent(slotTransform);
+
+                    //instance.transform.localPosition = new Vector3(slot.transform.position.x, slot.transform.position.y, slot.transform.position.z);
+
+                    // sets the instance's position to be 0 0 0.
+                    instance.transform.localPosition = new Vector3(0, 0, 0);
+
+                    Debug.Log("Set pos of instantiated object");
+                }
+                else
+                {
+                    Debug.LogError("Failed to load and instantiate the GameObject.");
+                }
+            }
+            else
+            {
+                Debug.Log("Slots to load are empty");
+            }
+
+            // Put back when trying to Code  + inventory.slots[i].transform.GetChild(0).name.Replace("(Clone)", "")
+            // Put In instantiate SlotName
+
+     
+
+            //print("inventorySlots: " + inventory.slots[i].transform.GetChild(0).name);
+
+            // Potentially a better way to get the right name?
+            //var clone = Instantiate(inventory.slots[i].transform.GetChild(i));
+            //clone.name = inventory.slots[i].transform.GetChild(i).name;
+
+            //string Testingg = SaveInventory[i].transform.GetChild(0).name.Replace("(Clone)", "");
+
 
 
             //prefabName[i] = SaveInventoryName[i];
 
-            Debug.Log("StartInstantiateInventory");
+            //Debug.Log("StartInstantiateInventory");
 
             //Problem here. And Problem with referencing Prefabs in inspector doesn't really work from another prefab.
             /*if (prefabDictionary.TryGetValue(SaveInventoryName[i], out GameObject parent))
